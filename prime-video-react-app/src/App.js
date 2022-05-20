b5d1a78834aa4f7b77f4ef3ae1fe475dc61fae6d
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-05-20 12:30:21 +0530

    updated

---
+++ prime-video-react-app/src/App.js
@@ -0,0 +1,23 @@
+/*
+  1. import statements 
+  2. comp defn 
+      * should return JSX 
+      * JSX will look like HTML -- but not HTML 
+  3. export 
+*/
+
+// imports 
+import './App.css';
+
+// comp defn 
+function App() {
+  // comp should return JSX 
+  return (
+    <div>
+      <h1>Success</h1>
+    </div>
+  );
+}
+
+// export 
+export default App;
---
+++ prime-video-react-app/src/index.js
@@ -0,0 +1,20 @@
+// node_module imports
+import React from 'react'; // official react library
+import ReactDOM from 'react-dom'; // react-dom package for working with the DOM.
+
+//custom imports
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// using React DOM package
+// app comp is rendered into an element that has id root
+ReactDOM.render(
+  <App />
+, document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
